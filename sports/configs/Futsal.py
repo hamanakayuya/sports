1b6a5bab9523a69d3a18dedd9e9d244a6af2885c
hamanakayuya <c1200988@st.kanazawa-it.ac.jp>
2025-07-14 12:27:33 +0900

    フットサル

--- sports/configs/Futsal.py
+++ sports/configs/Futsal.py
@@ -1,4 +1,3 @@
-# sports/configs/Futsal.py
 from dataclasses import dataclass, field
 from typing import List, Tuple
 import cv2
@@ -17,41 +16,40 @@ class FutsalPitchConfiguration:
     @property
     def vertices(self) -> List[Tuple[float, float]]:
         return [
-            (0, 0), (0, self.width), (self.length, self.width), (self.length, 0),
-            (self.length / 2, 0), (self.length / 2, self.width)
+            (0,0), (0,self.width), (self.length,self.width), (self.length,0),
+            (self.length/2,0), (self.length/2,self.width)
         ]
 
-    edges: List[Tuple[int, int]] = field(
-        default_factory=lambda: [(0,1),(1,2),(2,3),(3,0),(4,5)]
+    edges: List[Tuple[int,int]] = field(
+        default_factory=lambda:[(0,1),(1,2),(2,3),(3,0),(4,5)]
     )
 
-    # --- util ---
-    def _px(self, p: Tuple[float, float]) -> Tuple[int, int]:
-        return (int(p[0]*self.scale + self.offset[0]),
-                int(p[1]*self.scale + self.offset[1]))
+    # -------- util ----------
+    def _px(self, p:Tuple[float,float])->Tuple[int,int]:
+        return (int(p[0]*self.scale+self.offset[0]),
+                int(p[1]*self.scale+self.offset[1]))
 
-    # --- extra lines ---
+    # -------- 追加描画 -------
     def draw_extras(self, img):
         # センターサークル
         cv2.circle(img, self._px((self.length/2, self.width/2)),
                    int(self.centre_circle_radius*self.scale),
                    (255,255,255), 2)
 
-        # 左右ペナルティマーク & アーク
+        # ペナルティマーク & アーク
         for x in (self.penalty_spot_distance,
                   self.length-self.penalty_spot_distance):
             spot = self._px((x, self.width/2))
             cv2.circle(img, spot, 4, (255,255,255), -1)
-            start, end = (270,90) if x < self.length/2 else (90,270)
+            start,end = (270,90) if x < self.length/2 else (90,270)
             cv2.ellipse(img, spot,
                         (int(self.penalty_arc_radius*self.scale),)*2,
                         0, start, end, (255,255,255), 2)
 
         # ゴール縦線
-        g_half = self.goal_width/2
+        g = self.goal_width/2
         for x in (0, self.length):
-            cv2.line(img,
-                     self._px((x, self.width/2-g_half)),
-                     self._px((x, self.width/2+g_half)),
-                     (255,255,255), 2)
+            cv2.line(img, self._px((x, self.width/2-g)),
+                          self._px((x, self.width/2+g)),
+                          (255,255,255), 2)
         return img
